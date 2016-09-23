52afc9eeebec1807be12ffab19a363fff45f685b
Théo FIDRY <theo.fidry@gmail.com>
2016-09-23 19:42:37 +0100

    Add more tests for relationships (#4)

---
+++ fixtures/Bridge/Symfony/Entity/Group.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Fidry\AliceDataFixtures package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fidry\AliceDataFixtures\Bridge\Symfony\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class Group
+{
+    private $id;
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function setUsers(array $users)
+    {
+        foreach ($users as $user) {
+            $this->addUser($user);
+        }
+    }
+
+    public function addUser(User $user)
+    {
+        if (false === $this->users->contains($user)) {
+            $this->users->add($user);
+        }
+    }
+}
--- tests/Bridge/Symfony/Doctrine/ORMLoaderIntegrationTest.php
+++ tests/Bridge/Symfony/Doctrine/ORMLoaderIntegrationTest.php
@@ -11,8 +11,11 @@
 
 namespace Fidry\AlicePersistence\Bridge\Symfony\Doctrine;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Fidry\AliceDataFixtures\Bridge\Symfony\Entity\Dummy;
+use Fidry\AliceDataFixtures\Bridge\Symfony\Entity\Group;
+use Fidry\AliceDataFixtures\Bridge\Symfony\Entity\User;
 use Fidry\AliceDataFixtures\Bridge\Symfony\SymfonyApp\DoctrineOrmKernel;
 use Fidry\AliceDataFixtures\LoaderInterface;
 use PHPUnit\Framework\TestCase;
@@ -23,23 +26,60 @@ use PHPUnit\Framework\TestCase;
  */
 class ORMLoaderIntegrationTest extends TestCase
 {
-    public function testLoadAFile()
+    /**
+     * @var DoctrineOrmKernel
+     */
+    private $kernel;
+
+    /**
+     * @var LoaderInterface
+     */
+    private $loader;
+
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    public function setUp()
     {
-        $kernel = new DoctrineOrmKernel('test', true);
-        $kernel->boot();
+        $this->kernel = new DoctrineOrmKernel('test', true);
+        $this->kernel->boot();
 
-        /** @var LoaderInterface $loader */
-        $loader = $kernel->getContainer()->get('fidry_alice_data_fixtures.loader');
-        $loader->load([__DIR__.'/../../../../fixtures/fixture_files/dummy.yml']);
+        $this->loader = $this->kernel->getContainer()->get('fidry_alice_data_fixtures.loader');
+        $this->doctrine = $this->kernel->getContainer()->get('doctrine');
+    }
 
-        $doctrine = $kernel->getContainer()->get('doctrine');
-        $result = $doctrine->getRepository(Dummy::class)->findAll();
-
-        $this->assertEquals(1, count($result));
-
-        $purger = new ORMPurger($doctrine->getManager());
+    public function tearDown()
+    {
+        $purger = new ORMPurger($this->doctrine->getManager());
         $purger->purge();
 
-        $kernel->shutdown();
+        $this->kernel->shutdown();
+        $this->kernel = null;
+    }
+
+    public function testLoadAFile()
+    {
+        $this->loader->load([
+            __DIR__.'/../../../../fixtures/fixture_files/dummy.yml',
+        ]);
+
+        $result = $this->doctrine->getRepository(Dummy::class)->findAll();
+
+        $this->assertEquals(1, count($result));
+    }
+
+    public function testBidirectionalRelationships()
+    {
+        $this->loader->load([
+            __DIR__.'/../../../../fixtures/fixture_files/user_group.yml',
+        ]);
+
+        $users = $this->doctrine->getRepository(User::class)->findAll();
+        $groups = $this->doctrine->getRepository(Group::class)->findAll();
+
+        $this->assertEquals(10, count($users));
+        $this->assertEquals(10, count($groups));
     }
 }
