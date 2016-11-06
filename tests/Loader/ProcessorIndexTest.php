b441109d53a9f955b03f06a27a2a331fb30fc78f
Théo FIDRY <theo.fidry@gmail.com>
2016-11-06 18:24:04 +0000

    Pass the fixture ID to the processors

---
+++ tests/Loader/ProcessorIndexTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Fidry\AliceDataFixtures package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Fidry\AliceDataFixtures\Loader;
+
+use Fidry\AliceDataFixtures\Persistence\Persister\NullPersister;
+use Fidry\AliceDataFixtures\ProcessorInterface;
+use Nelmio\Alice\Loader\NativeLoader;
+
+/**
+ * @coversNothing
+ *
+ * @author Théo FIDRY <theo.fidry@gmail.com>
+ */
+class ProcessorIndexTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndexesArePreserved()
+    {
+        $processor = new DummyProcessor();
+        $loader = new PersisterLoader(
+            new SimpleLoader(
+                new NativeLoader()
+            ),
+            new NullPersister(),
+            [$processor]
+        );
+
+        $loader->load([__DIR__.'/../../fixtures/fixture_files/dummy.yml']);
+
+        $this->assertSame(['dummy0'], $processor->preIds);
+        $this->assertSame(['dummy0'], $processor->postIds);
+    }
+}
+
+class DummyProcessor implements ProcessorInterface
+{
+    public $preIds = [];
+    public $postIds = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function preProcess(string $id, $object)
+    {
+        $this->preIds[] = $id;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function postProcess(string $id, $object)
+    {
+        $this->postIds[] = $id;
+    }
+}
---
+++ tests/Persistence/Persister/NullPersisterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Fidry\AliceDataFixtures package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Fidry\AliceDataFixtures\Persistence\Persister;
+
+use Fidry\AliceDataFixtures\Persistence\PersisterInterface;
+
+/**
+ * @covers \Fidry\AliceDataFixtures\Persistence\Persister\NullPersister
+ *
+ * @author Théo FIDRY <theo.fidry@gmail.com>
+ */
+class NullPersisterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsAPersister()
+    {
+        $this->assertTrue(is_a(NullPersister::class, PersisterInterface::class, true));
+    }
+}
