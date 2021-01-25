020d315c291a6617ccd0a3a2aad8a1ef7cec8235
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2021-01-25 18:42:58 +0100

    Revert "Remove dependencies not compatible with PHP 8"
    This reverts commit 6dc3c966097bff8e0ed92d5f4abba81cee6f782d.

--- tests/Bridge/Symfony/Doctrine/MongodbLoaderIntegrationTest.php
+++ tests/Bridge/Symfony/Doctrine/MongodbLoaderIntegrationTest.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace Fidry\AlicePersistence\Bridge\Symfony\Doctrine;
 
-use Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle;
 use Doctrine\Common\DataFixtures\Purger\MongoDBPurger;
 use Doctrine\Persistence\ManagerRegistry;
 use Fidry\AliceDataFixtures\Bridge\Symfony\MongoDocument\Dummy;
@@ -65,10 +64,6 @@ class MongodbLoaderIntegrationTest extends TestCase
      */
     public function setUp(): void
     {
-        if (!class_exists(DoctrineMongoDBBundle::class)) {
-            $this->markTestSkipped('Need doctrine/mongodb-odm-bundle package.');
-        }
-
         $this->kernel = new DoctrineMongodbKernel(static::$seed, true);
         $this->kernel->boot();
 
