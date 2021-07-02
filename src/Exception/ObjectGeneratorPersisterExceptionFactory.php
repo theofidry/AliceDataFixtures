1e5e46d3db7a59fb9dadf59446a2dea1cca39a54
Marion Hurteau <mhurteau@jolicode.com>
2021-07-02 11:52:37 +0200

    Fix typo

--- src/Exception/ObjectGeneratorPersisterExceptionFactory.php
+++ src/Exception/ObjectGeneratorPersisterExceptionFactory.php
@@ -25,7 +25,7 @@ final class ObjectGeneratorPersisterExceptionFactory
     {
         return new LogicException(sprintf('No ID found for the entity "%1$s". If this entity has an auto ID generator, ' .
                 'this may be due to having it disabled because one instance of the entity had an ID assigned. ' .
-                'Either remove this assigned ID to allow the auto ID generator to operate or generate and ID for ' .
+                'Either remove this assigned ID to allow the auto ID generator to operate or generate an ID for ' .
                 'all the "%1$s" entities.',
                 get_class($entity)
             ));
