553f56d286b58bd284cb294009f2d5b9b01f948a
Aurélien Fontaine <9930567+Livda@users.noreply.github.com>
2021-12-19 17:45:20 +0100

    Remove deprecation for Symfony 5.4 (#189)

--- src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -29,7 +29,7 @@ final class Configuration implements ConfigurationInterface
     /**
      * @inheritdoc
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('fidry_alice_data_fixtures');
         if (method_exists($treeBuilder, 'getRootNode')) {
