d7a676f8460df34e4a4fb8cd1b59312b787f4992
Lluis Santos <lluis@ngynx.com>
2022-02-02 16:07:17 +0100

    fix style

--- src/Bridge/Doctrine/Persister/ObjectManagerPersister.php
+++ src/Bridge/Doctrine/Persister/ObjectManagerPersister.php
@@ -154,7 +154,7 @@ class ObjectManagerPersister implements PersisterInterface
                 foreach ($fieldValueOrFieldValues->getValues() as $fieldValue) {
                     $this->getMetadata($targetEntityClassName, $fieldValue);
                 }
-            } elseif (is_array($fieldValueOrFieldValues)){
+            } elseif (is_array($fieldValueOrFieldValues)) {
                 foreach ($fieldValueOrFieldValues as $fieldValue) {
                     $this->getMetadata($targetEntityClassName, $fieldValue);
                 }
