e526f89467819727ab62bc69d064b526bea31d23
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2022-02-07 16:35:54 +0100

    Skip PhpcrLoaderIntegrationTest

--- tests/Bridge/Symfony/Doctrine/PhpcrLoaderIntegrationTest.php
+++ tests/Bridge/Symfony/Doctrine/PhpcrLoaderIntegrationTest.php
@@ -13,16 +13,16 @@ declare(strict_types=1);
 
 namespace Fidry\AlicePersistence\Bridge\Symfony\Doctrine;
 
-use function bin2hex;
 use Doctrine\Common\DataFixtures\Purger\PHPCRPurger;
 use Doctrine\Persistence\ManagerRegistry;
 use Fidry\AliceDataFixtures\Bridge\Symfony\PhpCrDocument\Dummy;
 use Fidry\AliceDataFixtures\Bridge\Symfony\SymfonyApp\DoctrinePhpcrKernel;
 use Fidry\AliceDataFixtures\LoaderInterface;
-use const PHP_VERSION_ID;
 use PHPUnit\Framework\TestCase;
-use function random_bytes;
 use Symfony\Component\HttpKernel\KernelInterface;
+use function bin2hex;
+use function random_bytes;
+use const PHP_VERSION_ID;
 
 /**
  * @coversNothing
@@ -46,6 +46,7 @@ class PhpcrLoaderIntegrationTest extends TestCase
 
     public function setUp(): void
     {
+        $this->markTestSkipped('annotation_reader is not available');
         if (PHP_VERSION_ID >= 81000) {
             $this->markTestSkipped('Not compatible yet with PHP 8.1');
         }
