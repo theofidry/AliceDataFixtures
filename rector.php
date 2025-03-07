32d9ce8433a9e72e0ec875a37569286cd08a955c
Théo FIDRY <theo.fidry@gmail.com>
2025-03-07 09:54:08 +0100

    wip

---
+++ rector.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the humbug/php-scoper package.
+ *
+ * Copyright (c) 2017 Théo FIDRY <theo.fidry@gmail.com>,
+ *                    Pádraic Brady <padraic.brady@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/fixtures',
+        __DIR__.'/migrations',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withAutoloadPaths([
+        __DIR__.'/vendor/autoload.php',
+        __DIR__.'/vendor-bin/rector/vendor/autoload.php',
+    ])
+    ->withImportNames(removeUnusedImports: true)
+    ->withPhpSets(php83: true)
+    ->withAttributesSets(phpunit: true)
+    ->withSkip([
+//        __DIR__.'/composer-root-version-checker/vendor',
+//
+//        __DIR__.'/src/PhpParser/TraverserFactory.php',
+//        __DIR__.'/tests/PhpParser/UseStmtNameTest.php',
+//        __DIR__.'/src/PhpParser/NodeVisitor/AttributeAppender/ParentNodeAppender.php',
+//
+//        Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
+//        Rector\Php73\Rector\String_\SensitiveHereNowDocRector::class,
+//        Rector\Php81\Rector\ClassMethod\NewInInitializerRector::class => [
+//            __DIR__.'/src/Configuration/Configuration.php',
+//        ],
+//        Rector\Php81\Rector\Property\ReadOnlyPropertyRector::class => [
+//            __DIR__.'/src/Configuration/Configuration.php',
+//        ],
+//        Rector\Php55\Rector\String_\StringClassNameToClassConstantRector::class => [
+//            __DIR__.'/tests/Symbol/NamespaceRegistryTest.php',
+//            __DIR__.'/tests/Symbol/Reflector/UserSymbolsReflectorTest.php',
+//            __DIR__.'/tests/Symbol/SymbolRegistryTest.php',
+//            __DIR__.'/tests/Symbol/SymbolsRegistryTest.php',
+//        ],
+    ]);
